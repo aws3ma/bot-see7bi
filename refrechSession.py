dd19c66aff05e0aa583a0d687f6135599d8ee74d
amin benSlimen <aminbenslimen00@gmail.com>
2021-09-14 18:15:24 +0100

    first commit

---
+++ refrechSession.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from threading import Thread , Timer
+from datetime import datetime
+
+app = Flask('')
+
+@app.route('/')
+def home():
+  return '<h1>wa 9adar bot</h1>'
+def run():
+  app.run(host='0.0.0.0',port=8080)
+
+def keepAlive():
+  t = Thread(target=run)
+  t.start()
+
+
+
